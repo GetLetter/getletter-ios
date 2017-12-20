e0a8f89f43d68e74747ace9949a149caf99bf715
CMarzin <corentinmarzin@Gmail.com>
2017-12-20 14:12:48 +0100

    [ADD] add navigation

---
+++ getLetter/getLetter/BasicFlowViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BasicFlowViewController.swift
+//  getLetter
+//
+//  Created by Corentin Marzin on 20/12/2017.
+//  Copyright © 2017 getLetter. All rights reserved.
+//
+
+import UIKit
+
+class BasicFlowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func BackButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ getLetter/getLetter/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  getLetter
+//
+//  Created by Corentin Marzin on 20/12/2017.
+//  Copyright © 2017 getLetter. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIPageViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        print("ca marche ?")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
